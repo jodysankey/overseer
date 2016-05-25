a4a30329af3fa002260f7ac3d368c0436d1755bc
Jody Sankey <jody@jsankey.com>
2016-05-24 23:26:55 -0700

    Add command runner and unit test.

---
+++ src/main/java/com/jsankey/overseer/CommandRunner.java
@@ -0,0 +1,157 @@
+package com.jsankey.overseer;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.logging.Logger;
+
+import javax.annotation.Nullable;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+
+/**
+ * Initiates, terminates, and monitors execution of a given command.
+ * 
+ * <p>This class is thread safe.
+ * 
+ * @author Jody
+ */
+public class CommandRunner {
+  
+  /** Logger for the current class. */
+  private static final Logger LOG = Logger.getLogger(CommandRunner.class.getCanonicalName());
+  /** The bit bucket as a {@link File} for convenience of redirects. */
+  private static final File DEVNULL = new File("/dev/null");
+
+  // Immutable state
+  private final Clock clock;
+  private final String command;
+  private final ProcessBuilder builder;
+
+  // Current execution
+  @Nullable private Process process;
+  @Nullable private Instant startTime;
+
+  // Previous execution
+  @Nullable private CommandEvent lastEvent; 
+
+  private CommandRunner(String command, Clock clock) {
+    this.command = command;
+    // TODO(jody): We accept a complete command string in the command line arguments but
+    // need to tokenize for starting the process. For now very crudely split at every space,
+    // could do something better than this when we need it.
+    String[] commandArray = command.split("\\s");
+    this.clock = clock;
+
+    ProcessBuilder builder = new ProcessBuilder(commandArray)
+        .redirectInput(DEVNULL)
+        .redirectOutput(DEVNULL)
+        .redirectError(DEVNULL);
+        
+    this.builder = builder;
+    this.process = null;
+    this.startTime = null;
+    this.lastEvent = null;
+  }
+
+  /**
+   * Constructs a new {@link CommandRunner} using the system clock.
+   * 
+   * @param command the command to be run
+   * @return a new {@link CommandRunner} instance
+   */
+  public static CommandRunner forCommand(String command) {
+    return new CommandRunner(command, Clock.systemUTC());
+  }
+
+  /**
+   * Constructs a new {@link CommandRunner} with the supplied clock.
+   * 
+   * @param command the command to be run
+   * @param clock the clock to use for reading time
+   * @return a new {@link CommandRunner} instance
+   */
+  @VisibleForTesting
+  static CommandRunner forCommand(String command, Clock clock) {
+    return new CommandRunner(command, clock);
+  }
+
+  /**
+   * Returns the {@link Clock} this instance uses to read time.
+   */
+  public Clock getClock() {
+    return clock;
+  }
+
+  /**
+   * Returns the command string this instance will run.
+   */
+  public String getCommand() {
+    return command;
+  }
+  
+  /**
+   * Begins execution of the command then returns immediately.
+   * 
+   * <p>Preconditions: The command is not already executing.
+   * @throws IOException 
+   */
+  public synchronized void start() throws IOException {
+    Preconditions.checkState(process == null, "Command is already running");  
+    startTime = clock.instant();
+    process = builder.start();
+    LOG.info("Starting command: " + command);
+  }
+  
+  /**
+   * Terminates execution of the command if it is still running.
+   */
+  public synchronized void terminate() {
+    if (process !=null) {
+      process.destroy();
+      LOG.info("Terminating command: " + command);
+      lastEvent = new CommandEvent(startTime, clock.instant(), CommandEvent.ENFORCED_TERMINATION);
+      process = null;
+    }
+  }
+  
+  /**
+   * Returns true iff the command is currently executing.
+   * 
+   * <p>Note that polling this method periodically is used to measure when execution of a command
+   * completes without using a dedicated checker thread. Calling with higher frequency increases the
+   * accuracy of completion times.
+   */
+  public synchronized boolean isRunning() {
+    if (process != null) {
+      try {
+        int exitCode = process.exitValue();
+        // If we didn't throw an exception the command must now be finished
+        lastEvent = new CommandEvent(startTime, clock.instant(), exitCode);
+        process = null;
+        LOG.info(String.format("Detected completion of command: %s (%.2f seconds)",
+            command, lastEvent.getDurationMillis()/1000f));
+      } catch (IllegalThreadStateException e) {
+        // Normal that if the process is not done yet we receive this Exception.
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  /**
+   * Returns a {@link CommandEvent} representing the most recent completed execution.
+   * 
+   * <p>Preconditions:</p>
+   * <ul>The command is not currently executing</ul>
+   * <ul>One or more executions have been performed</ul>
+   */
+  public synchronized CommandEvent getLastExecution() {
+    Preconditions.checkState(process == null, "Cannot get last execution while command is running");  
+    Preconditions.checkState(lastEvent != null, "Cannot get last execution before completion");
+    return lastEvent;
+  }
+
+}
---
+++ src/test/java/com/jsankey/overseer/CommandRunnerTest.java
@@ -0,0 +1,81 @@
+package com.jsankey.overseer;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandRunnerTest {
+  
+  private static final String TEST_COMMAND = "false ignored argument";
+  private static final String INFINITE_COMMAND = "tail -F /dev/null";
+  
+  private static final Instant T1 = Instant.ofEpochSecond(55550001);
+  private static final Instant T2 = Instant.ofEpochSecond(55550002);
+
+  private CommandRunner testObject;
+  private Clock mockClock;
+
+  @Before
+  public void setUp() {
+    mockClock = mock(Clock.class);
+    testObject = CommandRunner.forCommand(TEST_COMMAND, mockClock);
+  }
+
+  @Test
+  public void testImmutableProperties() {
+    assertThat(testObject.getCommand()).isEqualTo(TEST_COMMAND);
+    assertThat(testObject.getClock()).isSameAs(mockClock);
+  }
+  
+  @Test
+  public void testNotYetRun() {
+    assertThat(testObject.isRunning()).isFalse();
+    try {
+      testObject.getLastExecution();
+      fail();
+    } catch (IllegalStateException e) {
+      // Expected
+    }
+  }
+
+  @Test
+  public void testExecution() throws Exception {
+    when(mockClock.instant()).thenReturn(T1, T2);
+    testObject.start();
+
+    // Allow time for false to complete
+    TimeUnit.MILLISECONDS.sleep(500);
+    assertThat(testObject.isRunning()).isFalse();
+    
+    CommandEvent lastExecution = testObject.getLastExecution();
+    assertThat(lastExecution.getExitCode()).isEqualTo(1);
+    assertThat(lastExecution.getStart()).isEqualTo(T1);
+    assertThat(lastExecution.getEnd()).isEqualTo(T2);
+  }
+  
+  @Test
+  public void testTermination() throws Exception {
+    when(mockClock.instant()).thenReturn(T1, T2);
+    testObject = CommandRunner.forCommand(INFINITE_COMMAND, mockClock);
+    testObject.start();
+
+    TimeUnit.MILLISECONDS.sleep(250);
+    assertThat(testObject.isRunning()).isTrue();
+    testObject.terminate();
+    TimeUnit.MILLISECONDS.sleep(250);
+    assertThat(testObject.isRunning()).isFalse();
+
+    CommandEvent lastExecution = testObject.getLastExecution();
+    assertThat(lastExecution.getExitCode()).isEqualTo(CommandEvent.ENFORCED_TERMINATION);
+    assertThat(lastExecution.getStart()).isEqualTo(T1);
+  }
+}
